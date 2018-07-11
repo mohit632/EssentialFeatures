a4b47c975462fc32a5961a5232e783f388b9b189
chandan vasishta <vasishta.chandan@gmail.com>
2018-07-11 20:54:16 +0530

    Actual commit

---
+++ EssentialFeatures/Models/LinqValueCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialFeatures.Models
+{
+    public class LinqValueCalculator
+    {
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(p => p.Price);
+        }
+
+        //Freeman, Adam.Pro ASP.NET MVC 5 (Expert's Voice in ASP.Net) (Kindle Locations 3896-3899). Apress. Kindle Edition. 
+    }
+}
---
+++ EssentialFeatures/Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialFeatures.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator calc;
+        public ShoppingCart(LinqValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        public IEnumerable<Product> Products { get; set; }
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+
+        //Freeman, Adam.Pro ASP.NET MVC 5 (Expert's Voice in ASP.Net) (Kindle Locations 3912-3919). Apress. Kindle Edition. 
+    }
+}
